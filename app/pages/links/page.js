e5fd132440e76a263010487bc0e4e6a69d70a662
Srinjay Kapri <srinjaayan1262000@gmail.com>
2024-02-08 15:47:15 +0530

    short id

--- app/pages/links/page.js
+++ app/pages/links/page.js
@@ -76,7 +76,7 @@ function Links() {
         </div>
       </div>
 
-      {showResult && baseUrl.length !== 0 &&
+      {showResult && baseUrl.length !== 0 &&shortId!==""&&
         <div className="container mt-3" id="resultDiv">
           <div className="row justify-content-center">
             <div className="col-md-6">
